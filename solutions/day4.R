26d9780fcdac8c66cee1a21bb21e33421bb6a40b
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2018-12-05 22:22:48 +0100

    Solutions and data for day 4 problems.

---
+++ solutions/day4.R
@@ -0,0 +1,98 @@
+library(tidyverse)
+library(lubridate)
+## input
+
+test <- c(
+"[1518-11-01 00:00] Guard #10 begins shift",
+"[1518-11-01 00:05] falls asleep",
+"[1518-11-01 00:25] wakes up",
+"[1518-11-01 00:30] falls asleep",
+"[1518-11-01 00:55] wakes up",
+"[1518-11-01 23:58] Guard #99 begins shift",
+"[1518-11-02 00:40] falls asleep",
+"[1518-11-02 00:50] wakes up",
+"[1518-11-03 00:05] Guard #10 begins shift",
+"[1518-11-03 00:24] falls asleep",
+"[1518-11-03 00:29] wakes up",
+"[1518-11-04 00:02] Guard #99 begins shift",
+"[1518-11-04 00:36] falls asleep",
+"[1518-11-04 00:46] wakes up",
+"[1518-11-05 00:03] Guard #99 begins shift",
+"[1518-11-05 00:45] falls asleep",
+"[1518-11-05 00:55] wakes up"
+)
+
+input <- read_lines("input/day4.txt")
+
+format_input <- function(x) {
+  x %>% 
+    tibble(raw = .) %>% 
+    mutate(time      = str_replace_all(raw, "\\[|\\].*", "") %>% ymd_hm(), 
+           new.guard = str_detect(raw, "Guard"), 
+           guard     = str_extract(raw, "Guard #[0-9]*") %>% str_remove("Guard #") %>% as.integer(), 
+           sleeps    = str_detect(raw, "falls asleep"), 
+           wakes     = str_detect(raw, "wakes"), 
+           minute    = minute(time)) %>% 
+    arrange(time) %>% 
+    mutate(block = cumsum(new.guard)) %>% 
+    group_by(block) %>% 
+    nest() %>% 
+    mutate(guard     = map_int(data, ~ .$guard[1]),
+           data      = map(data, ~ .[-1, ]), 
+           day       = map(data, ~ round_date(.$time[1], unit = "day"))) %>% 
+    unnest(day) %>% 
+    mutate(intervals = map(data, ~ tibble(start = .$minute[.$sleeps], end = .$minute[.$wakes]))) %>% 
+    unnest(intervals)
+}
+
+solve_problem_1 <- function(input) {
+  format_input(input) %>% 
+    group_by(guard) %>% 
+    nest() %>% 
+    mutate(
+      ## total sleep duration 
+      sleep.duration = map_int(data, ~ sum(.$end - .$start)), 
+      ## create hour long vector and record how many times the guard was asleep at any given minute
+      distribution = map(data, function(data) {
+        time <- rep(0, 60)
+        pwalk(data %>% select(start, end), function(start, end) { time[(start+1):end] <<- time[(start+1):end]+1 })
+        time
+      }), 
+      ## most slept minute
+      most.sleepy = map_int(distribution, which.max) - 1, 
+      result = most.sleepy * guard
+    )  %>% 
+    arrange(desc(sleep.duration)) %>% 
+    slice(1) %>% 
+    pull(result)  
+}
+
+solve_problem_2 <- function(input) {
+  format_input(input) %>% 
+    group_by(guard) %>% 
+    nest() %>% 
+    mutate(
+      ## total sleep duration 
+      sleep.duration = map_int(data, ~ sum(.$end - .$start)), 
+      ## create hour long vector and record how many times the guard was asleep at any given minute
+      distribution = map(data, function(data) {
+        time <- rep(0L, 60)
+        pwalk(data %>% select(start, end), function(start, end) { time[(start+1):end] <<- time[(start+1):end]+1L })
+        time
+      }), 
+      ## most slept minute
+      most.sleepy = map_int(distribution, which.max) - 1, 
+      ## number of times it is slept
+      nb.sleeps = map_int(distribution, max), 
+      result = most.sleepy * guard
+    )  %>% 
+    arrange(desc(nb.sleeps)) %>% 
+    slice(1) %>% 
+    pull(result)  
+}
+
+## solve_problem_1(test)
+solve_problem_1(input) ## 0.34 s, 39698
+
+## solve_problem_2(test)
+solve_problem_2(input) ## 0.29s, 14920
